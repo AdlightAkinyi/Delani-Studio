a5593fa3c2771b3fab61bf70c2f6adcea1a571ff
C0159412020 <adlightakinyi1@gmail.com>
2022-03-15 10:21:48 +0300

    git commit - m" added live link"

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
-$(document).ready(function() {
-
-    //what we do section toggle
-    $('#design').click(function() {
-        $(this).hide();
-        $('.design').show();
-    });
-    $('.design').click(function() {
-        $(this).hide();
-        $('#design').show();
-    });
-    $('#dvpt').click(function() {
-        $(this).hide();
-        $('.dvpt').toggle();
-    });
-    $('.dvpt').click(function() {
-        $(this).hide();
-        $('#dvpt').toggle();
-    });
-    $('#product').click(function() {
-        $(this).hide();
-        $('.product').toggle();
-    });
-    $('.product').click(function() {
-        $(this).hide();
-        $('#product').toggle();
-    });
-
-    //portfolio overlays
+     //portfolio overlays
     $(".one").mouseover(function() {
         $("#one").show();
     });
